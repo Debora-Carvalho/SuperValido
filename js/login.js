a3068f602bfc2701f034be743d48f90065362aa2
AnahiMamani <narielanahi@gmail.com>
2024-06-14 20:39:30 -0300

    detalles

--- js/login.js
+++ js/login.js
@@ -4,13 +4,12 @@
             var email = document.getElementById('email').value;
             var senha = document.getElementById('senha').value;
 
-            if (email === 'test@gmail.com' && senha === '123456789') {
-                window.location.href = 'indexperfil.html'; // Redireciona para a página indexperfil
+            if (email === 'anahi@gmail.com' && senha === '1234') {
+                window.location.href = 'indexPerfil.html'; // Redireciona para a página indexperfil
             } else {
                 alert('E-mail ou senha incorretos.'); // Exibe uma mensagem de erro
             }
         }
-
         document.addEventListener('DOMContentLoaded', function() {
             var form = document.querySelector('form');
             form.addEventListener('submit', validarLogin);
